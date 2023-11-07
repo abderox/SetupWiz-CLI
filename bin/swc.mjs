60d43bcee420fecd0406016ded4e2a94475359f7
AbdelhadiMou <abdelhadi.mouzafir@adria-bt.com>
2023-11-07 17:41:57 +0100

    Update version numbers in package and binary files

--- bin/swc.mjs
+++ bin/swc.mjs
@@ -10,7 +10,7 @@ import { init } from './init.mjs';
 import { killProcessByPort, lookForProcessByPort } from '../lib/commands/process.mjs';
 
 
-const version = "1.1.0";
+const version = "1.1.1";
 
 const {
   gitVersion,
