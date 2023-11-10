2bdd953d7e23f1cdb6fa077f0a6680c085b0864f
AbdelhadiMou <abdelhadi.mouzafir@adria-bt.com>
2023-11-10 17:40:16 +0100

    fixing bug, building APIs

--- bin/swc.mjs
+++ bin/swc.mjs
@@ -10,7 +10,7 @@ import { init } from './init.mjs';
 import { killProcessByPort, lookForProcessByPort } from '../lib/commands/process.mjs';
 
 
-const version = "1.2.1";
+const version = "1.2.2";
 
 const {
   gitVersion,
